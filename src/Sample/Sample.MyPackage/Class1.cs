059f330d769506136f6ae7fd5f7b085337c2266b
Azure Pipeline <kam@laganlabs.it>
2020-07-02 10:42:33 +0100

    initial commit

--- src/Sample/Sample.MyPackage/Class1.cs
+++ src/Sample/Sample.MyPackage/Class1.cs
@@ -4,5 +4,9 @@ namespace Sample.MyPackage
 {
     public class Class1
     {
+        public string MyTestMethod()
+        {
+            return "welcome to my test method in my test package!";
+        }
     }
 }
